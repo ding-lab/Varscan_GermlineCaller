0153b5bf6509187be02671f824fdfb7b9f178658
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2020-01-29 14:59:20 -0600

    Initial development of script

--- src/process_sample.sh
+++ src/process_sample.sh
@@ -1,41 +1,42 @@
 #/bin/bash
 
 read -r -d '' USAGE <<'EOF'
-Run GATK HaplotypeCaller and SelectVariants
+Run Varscan germline variant caller
 
 Usage: process_sample.sh [options] reference.fa input.bam 
  
 Options:
 -h: Print this help message
 -d : Dry run - output commands but do not execute them
--C HC_ARGS : pass args to `gatk HaplotypeCaller`
--R SV_SNP_ARGS : pass SV_SNP_ARGS to `gatk SelectVariants -select-type SNP -select-type MNP`
--S SV_INDEL_ARGS : pass SV_INDEL_ARGS to `gatk SelectVariants -select-type INDEL`
+-C MP_ARGS : pass args to `samtools mpileup`
+-D JAVA_ARGS : pass args to java
+-E VS_ARGS: pass args to mpileup2indel and mpileup2snp
 -L INPUT_INTERVAL : One or more genomic intervals over which to operate
-  This is passed verbatim to HaplotypeCaller -L INPUT_INTERVAL
+  This is passed verbatim to samtools mpileup -r INPUT_INTERVAL
 -l INTERVAL_LABEL : A short label for interval, used for filenames.  Default is INPUT_INTERVAL
 -o OUTD : Output directory [ ./output ]
+-N : Write output of samtools mpileup to intermediate file rather than use pipes
+
+General format of command is,
+samtools mpileup -q 1 -Q 13 BAM | java -jar VarScan.jar mpileup2indel - --min-coverage 3 --min-var-freq 0.10 --p-value 0.10 --strand-filter 1 --output-vcf 1 
 
 Output filenames:
-    OUTD/GATK.snp.XXX.vcf
-    OUTD/GATK.indel.XXX.vcf
+    OUTD/Varscan.snp.XXX.vcf
+    OUTD/Varscan.indel.XXX.vcf
 where XXX is given by INTERVAL_LABEL
 EOF
 
-# HaplotypeCaller documentation: https://gatk.broadinstitute.org/hc/en-us/articles/360037225632-HaplotypeCaller
-# SelectVariants documentation: https://gatk.broadinstitute.org/hc/en-us/articles/360037225432-SelectVariants
-
-source /opt/GATK_GermlineCaller/src/utils.sh
+source /opt/Varscan_GermlineCaller/src/utils.sh
 SCRIPT=$(basename $0)
 
-GATK="/gatk/gatk"
+JAR="/opt/VarScan.v2.3.8.jar"
 
 # Set defaults
 OUTD="./output"
 OUTVCF="final.SV.WGS.vcf"
 
 # http://wiki.bash-hackers.org/howto/getopts_tutorial
-while getopts ":hdC:R:S:L:l:o:" opt; do
+while getopts ":hdC:D:E:L:l:o:N" opt; do
   case $opt in
     h)
       echo "$USAGE"
@@ -45,13 +46,13 @@ while getopts ":hdC:R:S:L:l:o:" opt; do
       DRYRUN=1
       ;;
     C) # value argument
-      HC_ARGS="$HC_ARGS $OPTARG"
+      MP_ARGS="$MP_ARGS $OPTARG"
       ;;
-    R) # value argument
-      SV_SNP_ARGS="$SV_SNP_ARGS $OPTARG"
+    D) # value argument
+      JAVA_ARGS="$JAVA_ARGS $OPTARG"
       ;;
-    S) # value argument
-      SV_INDEL_ARGS="$SV_INDEL_ARGS $OPTARG"
+    E) # value argument
+      VS_ARGS="$VS_ARGS $OPTARG"
       ;;
     L) # value argument
       INPUT_INTERVAL="$OPTARG"
@@ -61,6 +62,9 @@ while getopts ":hdC:R:S:L:l:o:" opt; do
       ;;
     o) # value argument
       OUTD=$OPTARG
+      ;;
+    N)  # binary argument
+      NO_PIPE=1
       ;;
     \?)
       >&2 echo "Invalid option: -$OPTARG"
@@ -98,21 +102,51 @@ else
     IX="$INPUT_INTERVAL"
 fi
 
+VARSCAN="/usr/bin/java $JAVA_ARGS -jar $JAR "
+VS_ARGS="$VS_ARGS --min-coverage 3 --min-var-freq 0.10 --p-value 0.10 --strand-filter 1 --output-vcf 1"
+MP_ARGS="$MP_ARGS -q 1 -Q 13"
+SAMTOOLS="/opt/conda/bin/samtools"
+
 if [ "$INPUT_INTERVAL" ]; then
-    HC_ARGS="$HC_ARGS -L $INPUT_INTERVAL"
+    MP_ARGS="$MP_ARGS -r $INPUT_INTERVAL"
 fi
 
 mkdir -p $OUTD
 test_exit_status
 
-# Run HaplotypeCaller
-OUT1="$OUTD/GATK.raw.${IX}.vcf"
-CMD1="$GATK HaplotypeCaller -R $REF -I $BAM -O $OUT1 $HC_ARGS"
-run_cmd "$CMD1" $DRYRUN
+OUT_SNP="$OUTD/Varscan.snp.${IX}.vcf"
+LOG_SNP="$OUTD/Varscan.snp.${IX}.log"
+OUT_INDEL="$OUTD/Varscan.indel.${IX}.vcf"
+LOG_INDEL="$OUTD/Varscan.indel.${IX}.log"
+
+CMD1="$SAMTOOLS mpileup $MP_ARGS -f $REF $BAM" 
+CMD2="$VARSCAN mpileup2snp - $VS_ARGS > $OUT_SNP 2> $LOG_SNP"
+CMD3="$VARSCAN mpileup2snp - $VS_ARGS > $OUT_INDEL 2> $LOG_INDEL"
+
+if [ $NO_PIPE ]; then
+    OUT1="$OUTD/mpileup.${IX}.out"
+    CMD="$CMD1 > $OUT1"
+    >&2 echo No pipe: writing to $OUT1
+    run_cmd "$CMD" $DRYRUN
+    
+    CMD="cat $OUT1 | $CMD2 "
+    run_cmd "$CMD" $DRYRUN
+    
+    CMD="cat $OUT1 | $CMD3 "
+    run_cmd "$CMD" $DRYRUN
+
+else
+    >&2 echo Running with process substitution
+# https://unix.stackexchange.com/questions/40277/is-there-a-way-to-pipe-the-output-of-one-program-into-two-other-programs
+    CMD="$CMD1 | tee >($CMD2) | $CMD3"
+    run_cmd "$CMD" $DRYRUN
+fi
+
+#    shell: "samtools mpileup -q 1 -Q 13 -f {input.genome_fa} -r {params.chr} {input.bam} | varscan mpileup2snp - --min-coverage 3 --min-var-freq 0.10 --p-value 0.10 --strand-filter 1 --output-vcf 1 > {output} 2>{log}"
+#    shell: "samtools mpileup -q 1 -Q 13 -f {input.genome_fa} -r {params.chr} {input.bam} | varscan mpileup2indel - --min-coverage 3 --min-var-freq 0.10 --p-value 0.10 --strand-filter 1 --output-vcf 1 > {output} 2>{log}"
+
 
 # Run SelectVariants
-OUT2A="$OUTD/GATK.snp.${IX}.vcf"
-OUT2B="$OUTD/GATK.indel.${IX}.vcf"
 CMD2A="$GATK SelectVariants -R $REF -V $OUT1 -O $OUT2A -select-type SNP -select-type MNP $SV_SNP_ARGS"
 CMD2B="$GATK SelectVariants -R $REF -V $OUT1 -O $OUT2B -select-type INDEL $SV_INDEL_ARGS"
 run_cmd "$CMD2A" $DRYRUN
